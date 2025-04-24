e05ff3e9c09b661df17739b19f3f3d95ed913caf
Precious-Adeoye <contact.preciousadeoye@gmail.com>
2025-04-24 12:47:37 +0100

    ..

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -5,9 +5,13 @@
         static void Main(string[] args)
         {
             Dad dad = new Dad("Abdi", "AA", 45, "Abdi");
-            Child child = new Child("5.5", 20, dad);
+            Child child = new Child("5.5", 21, dad);
 
             child.Show();
+            
+            string message = "Hello World!";
+            int length = message.Length;
+            Console.WriteLine($"The length of the message is: {length}");
         }
     }
 }
